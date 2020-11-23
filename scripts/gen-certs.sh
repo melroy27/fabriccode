497bb31a29649c3c5ce2538d8fb4dc25520ac288
Melroy Rodrigues <rockstarodmel@gmail.com>
2020-11-23 19:32:52 +0300

    changes for new peer

--- scripts/gen-certs.sh
+++ scripts/gen-certs.sh
@@ -6,5 +6,5 @@ fabric-ca-client enroll -u http://admin:adminpw@ca-root:7054
 
 bash $SCRIPTDIR/crypto.sh orderer:default.svc.cluster.local:default.svc.cluster.local:3
 
-bash $SCRIPTDIR/crypto.sh peer:org1:default.svc.cluster.local:2
+bash $SCRIPTDIR/crypto.sh peer:org1:default.svc.cluster.local:3
 
